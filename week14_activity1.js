2f493ee41948ae64c9ae87620bc041f093fd69a4
ssabeeha2109-oss <s.sabeeha2109@gmail.com>
2026-07-22 13:00:03 +0800

    hi

--- week14_activity1.js
+++ week14_activity1.js
@@ -1,1 +1,6 @@
-
+function updateTime() {
+  const now = new Date();
+  const formatted = now.toLocaleTimeString(); 
+  document.getElementById("timeDisplay").textContent = formatted;
+}
+setInterval(updateTime, 1000);
